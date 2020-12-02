68432d0de66b93ed41e6e525ee59a7c9b427853a
Paul Olbrich <Paul.Olbrich@sennheiser.com>
2020-12-02 11:56:03 +0100

    collisionWrning with Circles

--- Schiffchen6/Models/Ship.cs
+++ Schiffchen6/Models/Ship.cs
@@ -19,7 +19,7 @@ namespace Schiffchen6.Models
 
         public VectorC vector { get; set; }
 
-        Line line;
+        Ellipse ellipse;
         bool lineAdded = false;
         bool collision = false;
 
@@ -32,11 +32,12 @@ namespace Schiffchen6.Models
             divisor = rnd.Next(155, 200);
             VectorC vectorC = new VectorC(Sea, serial, divisor);
             vector = vectorC;
-            line = new Line
-            {
-                Fill = Brushes.Purple,
+            ellipse = new Ellipse
+            {                
                 Stroke = Brushes.Red,
-                StrokeThickness = 1
+                StrokeThickness = 1,
+                Width = 100,
+                Height = 100
             };
             rect = new Rectangle
             {
@@ -58,14 +59,25 @@ namespace Schiffchen6.Models
                 double distance = Point.Subtract(p, p1).Length;
                 if (distance <= 100 && !ship.collision)
                 {
-                    line.X1 = p.X +(rect.Width/2);
-                    line.Y1 = p.Y + (rect.Height / 2);
-                    line.X2 = p1.X+ (rect.Width / 2);
-                    line.Y2 = p1.Y+ (rect.Height / 2);
+                    Canvas.SetLeft(ellipse, p.X-(ellipse.Width/2) + (rect.Width / 2));
+                    Canvas.SetTop(ellipse, p.Y - (ellipse.Height / 2) + (rect.Height / 2));
+                    //line.X1 = p.X +(rect.Width/2);
+                    //line.Y1 = p.Y + (rect.Height / 2);
+                    //line.X2 = p1.X+ (rect.Width / 2);
+                    //line.Y2 = p1.Y+ (rect.Height / 2);
                     if (!lineAdded)
                     {
-                        Sea.Children.Add(line);
+                        Sea.Children.Remove(this.rect);
+                        Sea.Children.Remove(ship.rect);
+                        Sea.Children.Add(ellipse);
+                        Sea.Children.Add(this.rect);
+                        Sea.Children.Add(ship.rect);
                         lineAdded = true;
+                    }
+                    if(lineAdded && distance >= 100)
+                    {
+                        Sea.Children.Remove(ellipse);
+
                     }
                 }
 
